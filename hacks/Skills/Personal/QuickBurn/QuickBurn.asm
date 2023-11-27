3689d31f9507983731e0aaf0e61424c5d3903c4d
Jester4872 <jamescurtesen@gmail.com>
2023-11-27 10:03:12 +0000

    Skill (QuickBurn)

--- hacks/Skills/Personal/QuickBurn/QuickBurn.asm
+++ hacks/Skills/Personal/QuickBurn/QuickBurn.asm
@@ -18,7 +18,7 @@
 @r14=#0x80289E9
 @r15=#0x8028A28
 
-push  {r0-r3}
+push  {r0-r4}
 ldr   r1,[r0,#0x0]                  @load the pointer to the unit's data
 ldrb  r1,[r1,#0x4]                  @load the unit's character ID
 cmp   r1,#0x03                      @compare against a chosen character ID
@@ -28,27 +28,30 @@ b     End
 Get_Turn_Count:
     ldr     r1,=#0x202BBF8          @load the chapter struct
     ldrh    r1,[r1,#0x10]           @get the turn number
+    mov     r4,#0x10                @starting value to apply for the skill
     cmp     r1,#0xF                 @set a maximum value for the turn count to compare against
     bgt     Cap_Turn_Count          @branch to set it to this maximum if it is greater
-    b       SlowBurn                @otherwise continue to apply the skill as usual
+    sub     r4,r1                   @reduce it by the turncount
+    b       QuickBurn               @otherwise continue to apply the skill as usual
 
 Cap_Turn_Count:
-    mov     r1,#0xF                 @cap the turn count at 15
-    b       SlowBurn                @branch to apply the skill
+    mov     r1,#0x10                @cap the turn count at 16
+    sub     r4,r1                   @reduce the starting value by the turncount
+    b       QuickBurn               @branch to apply the skill
 
-SlowBurn:
+QuickBurn:
     mov     r3,#0x60                @get the hit rate short for the battle structs
     ldrh    r2,[r0,r3]              @load the hit rate value
-    add     r2,r1                   @add the turn counter to the hit rate value
+    add     r2,r4                   @add the remaining boost to the hit rate value
     strh    r2,[r0,r3]              @store the new hit rate
     mov     r3,#0x62                @get the avoid short for the battle structs
     ldrh    r2,[r0,r3]              @load the avoid value
-    add     r2,r1                   @add the turn counter to the avoid value
+    add     r2,r4                   @add the remaining boost to the avoid value
     strh    r2,[r0,r3]              @store the new avoid
     b       End                     @we're finished now, so branch to the end
 
 End:
-    pop     {r0-r3}                 @restore the original register values
+    pop     {r0-r4}                 @restore the original register values
     mov     r5,r0                   @vanilla instruction
     ldr     r1,=#0x203A3D8          @vanilla instruction
     mov     r0,#0x20                @vanilla instruction
